87552d72718790e141945908a70dc2c9546465d3
Janybanny <codeberg@janybanny.moe>
2026-02-26 21:54:42 +0100

    Added Database scripts with docker

--- MediaRatingsPlatform.CommandLine/dbclean.sh
+++ MediaRatingsPlatform.CommandLine/dbclean.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-podman stop mrpdb 
-podman rm mrpdb 
+docker stop mrpdb 
+docker rm mrpdb 
 echo "Old Container Removed"
 sleep 2
 
-podman run -d --name mrpdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres
+docker run -d --name mrpdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres
 echo "New Container Started"
 sleep 2
-podman exec -i mrpdb createdb -U postgres mrpdb
+docker exec -i mrpdb createdb -U postgres mrpdb
 echo "New Clean Database created"
--- MediaRatingsPlatform.CommandLine/dbdata.sh
+++ MediaRatingsPlatform.CommandLine/dbdata.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
-podman stop mrpdb 
-podman rm mrpdb 
-echo "Old Container Removed"
+docker stop mrpdb 2>/dev/null || true
+docker rm mrpdb 2>/dev/null || true
+echo "Old Container Removed (if any exist)"
 sleep 2
 
-podman volume create mrpdb-data
+if ! docker volume inspect mrpdb-data > /dev/null 2>&1; then
+  docker volume create mrpdb-data
+fi
 
-podman run -d --name mrpdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v mrpdb-data:/var/lib/postgresql/data postgres
+docker run -d \
+  --name mrpdb \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=postgres \
+  -p 5432:5432 \
+  -v mrpdb-data:/var/lib/postgresql/data \
+  postgres:latest
 
 echo "New Container Started"
-sleep 2
+sleep 5
 
-podman exec -i mrpdb createdb -U postgres mrpdb
+# Wait for PostgreSQL to be ready before creating database
+docker exec mrpdb pg_isready -U postgres
+while [ $? -ne 0 ]; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 2
+  docker exec mrpdb pg_isready -U postgres
+done
+
+docker exec -i mrpdb createdb -U postgres mrpdb
 echo "Database with volume started"
